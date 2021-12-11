823760ca5605892baf7bc3cdcc0989506c6a971c
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-11 23:38:06 +0900

    [Array]

---
+++ array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array</title>
+</head>
+<body>
+    <?php
+    $arr = array('egoing', 'leezche', 'duru', 'select');
+    echo $arr[0];
+    echo $arr[1];
+    echo $arr[2];
+    echo $arr[3];
+
+    array_push($arr, 'sal', 'del');
+    
+    ?>
+</body>
+</html>
