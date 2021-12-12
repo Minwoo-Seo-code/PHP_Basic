2569159d06fbb13c6e7b0f5c6a6d2d62d997a77f
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-12 17:07:38 +0900

    [li tag php list]

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
-<!doctype html>
-<html>
-    <body>
-        <?php 
-            echo ('Hello World');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <h1><a href="index.php">WEB</a></h1>
+    <ol>
+        <?php
+            $list = scandir('./data');
+
+            $i = 0;
+            while($i < count($list)) {
+                if(!($list[$i] == '.') && !($list[$i] == '..')) {
+                    echo"<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+                }
+                $i += 1;
+            }
         ?>
-    </body>
+    </ol>
+</body>
 </html>
