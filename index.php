8bf19acbba4c34005116a6cf4bdc651ccdf23429
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-12 18:32:07 +0900

    [PHP function]

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-    <h1><a href="index.php">WEB</a></h1>
-    <ol>
-        <?php
-            $list = scandir('./data');
-
+<?php
+    function print_title() {
+        if (isset($_GET['id'])){
+            echo $_GET['id'];
+        }else {
+            echo "Welcome";
+        }
+    }
+    function print_description() {
+        if(isset($_GET['id'])) {
+            echo file_get_contents("data/".$_GET['id']);
+        } else {
+            echo "Hello, PHP";
+        }
+    }
+    function print_list() {
+        $list = scandir('./data');
             $i = 0;
             while($i < count($list)) {
                 if(!($list[$i] == '.') && !($list[$i] == '..')) {
@@ -19,7 +22,34 @@
                 }
                 $i += 1;
             }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php
+        print_title();
+        ?>
+    </title>
+</head>
+<body>
+    <h1><a href="index.php">WEB</a></h1>
+    <ol>
+        <?php
+            print_list();
         ?>
     </ol>
+    <h2>
+        <?php
+        print_title();
+        ?>
+    </h2>
+        <?php
+        print_description();
+        ?>
 </body>
 </html>
